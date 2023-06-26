c0b16f278ba434f3115d752a909c49e3d54e9a24
hibo <hiroaki.sato.031030@gmail.com>
2023-06-26 18:03:30 +0900

    飲み会設定画面作成

---
+++ tressaillir_app/resources/views/event-cordinator.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="jn">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+  <form action="{{ url('event') }}" method="post" class="">
+    @csrf
+    <div class="">
+      <div class="">
+        <label class="">
+          題名
+        </label>
+      <input name="event" class="" type="text" placeholder="">
+
+      <!-- カラム4 -->
+      <div class="">
+        <label class="">
+          日時
+        </label>
+      <input name="date" class="" type="text" placeholder="">
+
+      </div>
+        <label class="">
+          店名
+        </label>
+        <input name="event_place" class="" type="text" placeholder="">
+      </div>
+
+      <div class="">
+        <label class="">
+          マップURL
+        </label>
+        <input name="place_url" class="" type="text" placeholder="">
+      </div>
+
+      <!-- 送信ボタン -->
+      <div class="">
+        <div class="">
+          <button class="" type="submit">送信</button>
+        </div>
+      </div>
+    </div>
+  </form>
+</body>
+</html>
