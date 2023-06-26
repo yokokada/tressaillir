58ccb7b8ab01a2097ce4e26b759f55b193d208c1
s-sawa <s.s.hrmeee21@gmail.com>
2023-06-26 16:11:54 +0900

    席ランダム表示(2列)

--- tressaillir_app/resources/views/create.blade.php
+++ tressaillir_app/resources/views/create.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
