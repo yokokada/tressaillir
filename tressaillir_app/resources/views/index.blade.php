54a482abc0b254d95409b6ae936f3d4a0aae518f
yokokada <yokoumasaka@gmail.com>
2023-06-29 17:52:57 +0900

    api.php追加、index.php変更

--- tressaillir_app/resources/views/index.blade.php
+++ tressaillir_app/resources/views/index.blade.php
@@ -13,7 +13,7 @@
     
     <div class="flex justify-center items-center h-screen">
         <!--actual component start-->
-        <div x-data="setup()">
+        <div div x-data="setup()">
             {{-- タブ --}}
             <ul class="flex justify-center items-center my-4">
                 <template x-for="(tab, index) in tabs" :key="index">
@@ -22,76 +22,72 @@
                         x-text="tab"></li>
                 </template>
             </ul>
-            
-            <div class="w-80 bg-white p-16 text-center mx-auto border">
-            {{--  --}}
-            <div x-show="activeTab===0">
-                <div id="members-container" class="grid grid-cols-2 gap-4">
-                            @foreach ($members as $member)
+                 {{-- 以下参加者タブ --}}
+          <div class="w-80 bg-white p-16 text-center mx-auto border">
+                <div x-show="activeTab===0">
+                    <div id="members-container" class="grid grid-cols-2 gap-4">
+                                @foreach ($members as $member)
+                                <div class="flex items-center flex-col">
+                                    <p class="text-lg">{{ $member->nickname }}</p>
+                                    <img src="{{ asset($member->icon) }}" class="w-16 h-16 rounded-full mr-4">
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                </div>
+                 {{-- 以下席替えタブ --}}
+                <div x-show="activeTab===1">
+                  <div id="table-container">
+                        {{-- 総人数 --}}
+                        <h1> 総人数は{{ App\Models\Member::count() }}人です。</h1>
+                        <div id="container" >
+                            <h1>テーブル1 <input  input class="peopleInput" type="number" min="1" max="20" >人</h1>
+                        </div>
+                            <button class="add-button">　　　　テーブル追加</button>
+                        <br><br>
+                        <button id="distributeButton" >席決めスタート</button>
+                        <div class="set">
+                        <div id="resultContainer"></div>
+                      </div>
+                  </div>
+                </div>       
+                 {{-- 以下ファーストドリンクタブ --}}
+                <div x-show="activeTab===2">
+                        <div id="firstdrink-container">
+                            @php
+                                $uniqueFirstDrinks = $members->pluck('firstdrink')->unique();
+                            @endphp
+                        
+                            @foreach ($uniqueFirstDrinks as $firstDrink)
+                            @php
+                                $quantities = $members->where('firstdrink', $firstDrink)->count();
+                                $drinkName = [
+                                    '生ビール',
+                                    '瓶ビール',
+                                    'ノンアルコールビール',
+                                    'ハイボール',
+                                    'レモンサワー',
+                                    'ウーロンハイ',
+                                    '緑茶ハイ',
+                                    '烏龍茶',
+                                    'コーラ',
+                                    'ジンジャーエール',
+                                    'その他'
+                                ][$firstDrink];
+                            @endphp
                             <div class="flex items-center flex-col">
-                                <p class="text-lg">{{ $member->nickname }}</p>
-                                <img src="{{ asset($member->icon) }}" class="w-16 h-16 rounded-full mr-4">
+                                <p class="text-lg">{{ $drinkName }}　{{ $quantities }} 杯</p>
                             </div>
                             @endforeach
                         </div>
-                    </div>
-            </div>
-             <div x-show="activeTab===1">
-                    <div id="container">
-                            {{-- 総人数 --}}
-                        @php
-                            $totalRows = App\Models\Member::count();
-                            $additionalRange = 5; // 追加の範囲を指定してください
-                        @endphp
-                        <div>総人数は<select name="count">
-                        @for ($i = $totalRows - $additionalRange; $i <= $totalRows + $additionalRange; $i++)
-                            <option value="{{ $i }}">{{ $i }}</option>
-                        @endfor
-                        </select>人です
-                   </div>
-                        <h1>テーブル1 <input type="number" id="table-people1" min="1" max="15" >人</h1>
-                    </div>
-                    <button class="add-button">追加
-                    </button>
-
-                    <br>
-                    
-                    <button id="distributeButton" >席決めスタート</button><br>
-             </div>       
-             <div x-show="activeTab===2">
-                    <div id="firstdrink-container">
-                        @php
-                            $uniqueFirstDrinks = $members->pluck('firstdrink')->unique();
-                        @endphp
-                    
-                        @foreach ($uniqueFirstDrinks as $firstDrink)
-                        @php
-                            $quantities = $members->where('firstdrink', $firstDrink)->count();
-                            $drinkName = [
-                                '生ビール',
-                                '瓶ビール',
-                                'ノンアルコールビール',
-                                'ハイボール',
-                                'レモンサワー',
-                                'ウーロンハイ',
-                                '緑茶ハイ',
-                                '烏龍茶',
-                                'コーラ',
-                                'ジンジャーエール',
-                                'その他'
-                            ][$firstDrink];
-                        @endphp
-                        <div class="flex items-center flex-col">
-                            <p class="text-lg">{{ $drinkName }}　{{ $quantities }} 杯</p>
-                        </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
+                </div>     
+          </div>
         </div>
     </div>
-    
+
+    <!--  タブの切り替えJS -->
     <script>
+      
         function setup() {
         return {
           activeTab: 0,
@@ -104,43 +100,119 @@
       };
     </script>
 
-<script>
-    //テーブル追加する
-    document.addEventListener('DOMContentLoaded', function() {
-        // addボタンのクリックイベントを処理
-        document.addEventListener('click', function(event) {
-            if (event.target.classList.contains('add-button')) {
-                event.preventDefault();
-                // 行を複製して追加
-                var container = document.getElementById('container');
-                var row = document.querySelector('#container h1');
-                var newRow = row.cloneNode(true);
-                newRow.innerHTML = newRow.innerHTML.replace('テーブル1', 'テーブル' + (container.children.length + 1));
-                container.appendChild(newRow);
-            }
+   <!-- テーブル追加するJS -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // addボタンのクリックイベントを処理
+            document.addEventListener('click', function(event) {
+                if (event.target.classList.contains('add-button')) {
+                    event.preventDefault();
+                    // 行を複製して追加
+                    var container = document.getElementById('container');
+                    var row = document.querySelector('#container h1');
+                    var newRow = row.cloneNode(true);
+                    newRow.innerHTML = newRow.innerHTML.replace('テーブル1', 'テーブル' + (container.children.length + 1));
+                    container.appendChild(newRow);
+                }
+            });
         });
-    });
-</script>
+    </script>
+
+    <!-- 席分配JS -->
+      <script>
+          // 振り分けボタンを押すと、人数を振り分け
+          var distributeButton = document.getElementById('distributeButton');
+          distributeButton.addEventListener('click', function() {
+            // 総人数をfetch()関数を使用して取得
+            fetch('/api/members/total')
+            .then(response => response.json())
+            .then(data => {
+                const total = data.total;
+                console.log('総人数:', total);
+                // ここで必要な処理を行う
+            })
+            .catch(error => {
+                console.error('リクエストエラー:', error);
+            });
+
+            var peopleInputs = document.getElementsByClassName('peopleInput');
+            var peopleCounts = [];
+            var sum = 0;
+
+            for (var i = 0; i < peopleInputs.length; i++) {
+              var peopleInput = peopleInputs[i];
+              var peopleCount = parseInt(peopleInput.value);
+              peopleCounts.push(peopleCount);
+              sum += peopleCount;
+              console.log(sum);
+            }
+            //  sumは総人数
+            if (sum !== total) {
+              alert('入力された人数の合計が総人数と一致しません。');
+              return;
+            }
+           
+           // HTML要素の取得
+            var container = document.getElementById("memberContainer");
+            var members = Array.from(container.getElementsByClassName("member"));
+
+            // メンバー要素をシャッフル
+            shuffleArray(members);
+
+            
+            var resultContainer = document.getElementById('resultContainer');
+            resultContainer.innerHTML = '';
+            // 上記が結果表示の部分 
 
 
-<script>
-    function seatChange() {
-        var container = document.getElementById('members-container');
-        var members = Array.prototype.slice.call(container.children); // メンバー要素の配列を作成
+            var nameIndex = 0;
+            for (var i = 0; i < peopleCounts.length; i++) {
+              var peopleCount = peopleCounts[i];
+              var machineNumber = i + 1;
 
-        // 配列をシャッフル
-        for (var i = members.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = members[i];
-            members[i] = members[j];
-            members[j] = temp;
-        }
+             // 机
+              var desk = document.createElement('div');
+              desk.className = 'desk';
 
-        // シャッフル後のメンバー要素をコンテナに追加
-        for (var i = 0; i < members.length; i++) {
-            container.appendChild(members[i]);
-        }
-    }
-</script>
+              for (var j = 0; j < peopleCount; j++) {
+                if (nameIndex >= shuffledNames.length) {
+                  nameIndex = 0;
+                }
+               // 椅子
+                var chair = document.createElement('div');
+                chair.className = 'chair';
+
+                var nameLabel = document.createElement('div');
+                nameLabel.className = 'name-label';
+                nameLabel.innerHTML = shuffledNames[nameIndex];
+
+                chair.appendChild(nameLabel);
+                desk.appendChild(chair);
+
+                nameIndex++;
+              }
+
+              resultContainer.appendChild(desk);
+            }
+          });
+        
+          // 振り分け配列
+          function shuffleArray(array) {
+            var currentIndex = array.length;
+            var temporaryValue;
+            var randomIndex;
+
+            while (0 !== currentIndex) {
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex -= 1;
+
+              temporaryValue = array[currentIndex];
+              array[currentIndex] = array[randomIndex];
+              array[randomIndex] = temporaryValue;
+            }
+
+            return array;
+          }
+      </script>
 </body>
 </html>
